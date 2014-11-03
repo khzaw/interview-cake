f94f543c06b669c82754e874cb7b6d8246d7753e
Kaung Htet Zaw <emoosx@gmail.com>
2014-11-03 22:53:04 +0800

    Use only two parameters

--- making-change.py
+++ making-change.py
@@ -5,16 +5,18 @@
 # with coins of the available denominations.
 
 
-def num_of_ways(amount, denominations, m):
+def num_of_ways(amount, denominations):
+    # print "Amount :", amount, " Denominations:", denominations
     if amount == 0:
         return 1
     if amount < 0:
         return 0
-    if amount >= 1 and m < 0:
+    if len(denominations) == 0:
         return 0
-    return num_of_ways(amount, denominations, m-1) + \
-        num_of_ways(amount - denominations[m], denominations, m)
+    return num_of_ways(amount-denominations[0], denominations) + \
+        num_of_ways(amount, denominations[1:])
 
 
 if __name__ == '__main__':
-    print num_of_ways(20, [1, 2, 3], 2)
+    print num_of_ways(20, [19, 2])       # 1
+    print num_of_ways(4, [1, 2, 3])      # 4
